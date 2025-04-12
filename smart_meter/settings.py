185790de6878663680298de7dea2a10791d5c433
captainyt-max <swdcaptain@gmail.com>
2025-04-12 11:54:27 +0530

    migrated to postGRE SQL

--- smart_meter/settings.py
+++ smart_meter/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'smart_meter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smart_meter_db',
+        'USER': 'smart_meter_db_user',
+        'PASSWORD': 'lBGBBaFtgYVjemQ2xgGC55qmQo4gWVqj',
+        'HOST': 'dpg-cvt00hpr0fns73dsgpe0-a.oregon-postgres.render.com',
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
 
